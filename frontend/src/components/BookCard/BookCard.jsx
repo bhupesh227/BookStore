cca42874b7bd54a18ad05e58646d750b59b7d537
Bhupesh Bora <bhupeshbora01@gmail.com>
2025-03-19 23:19:17 +0530

    favourites functionality all books

--- frontend/src/components/BookCard/BookCard.jsx
+++ frontend/src/components/BookCard/BookCard.jsx
@@ -4,11 +4,11 @@ const BookCard = ({data}) => {
   return (
     <>
         <Link to={`/book-details/${data._id}`}>
-            <div className='bg-zinc-800 rounded p-4 flex flex-col'>
+            <div className='bg-zinc-800 rounded p-4 flex flex-col h-[100%]'>
                 <div className='bg-zinc-900 rounded flex items-center justify-center'>
                     <img src={data.url} alt="/" className='h-[30vh]'/>
                 </div>
-                <h2 className='mt-4 text-xl font-semibold h-[7vh] text-white'>{data.title}</h2>
+                <h2 className='mt-4 text-xl font-semibold h-[auto] text-white'>{data.title}</h2>
                 <p className='mt-2 text-zinc-400 font-semibold '>By {data.author}</p>
                 <p className='mt-2 text-zinc-200 font-semibold text-xl'>Rs. {data.price}</p>
             </div>
--- frontend/src/components/BookDetails/BookDetails.jsx
+++ frontend/src/components/BookDetails/BookDetails.jsx
@@ -3,10 +3,16 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios';
 import Loader from '../Loader/Loader';
 import { GrLanguage } from 'react-icons/gr'; 
+import { FaCartShopping, FaHeart } from 'react-icons/fa6';
+import { useSelector } from 'react-redux';
+import { MdModeEditOutline } from "react-icons/md";
+import { MdDeleteOutline } from "react-icons/md";
 
 const BookDetails = () => {
   const {id} = useParams();
   const [Data,setData] = useState();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const role = useSelector((state) => state.auth.role);
     useEffect(() => {
       const fetch = async () => {
           const response = await axios.get(`http://localhost:3000/api/v1/book-details/${id}`);
@@ -14,15 +20,63 @@ const BookDetails = () => {
       }
       fetch();
     }, [id]);
+
+    const headers = {
+        id: localStorage.getItem("id"),
+        authorization: `Bearer ${localStorage.getItem("token")}`,
+        bookid: id,
+    }
+    const handleFavourite = async () => {
+        try {
+           const response = await axios.put(`http://localhost:3000/api/v1/add-favourite`,{},{headers});
+            alert(response.data.message); 
+        } catch (error) {
+            if (error.response) {
+                alert(error.response.data.message || 'Something went wrong!');
+              } else {
+                alert('An unexpected error occurred.');
+              }
+        }  
+    }
   return (
     <>
         {Data && (
-            <div className='px-5 lg:px-12 py-8 bg-zinc-900 flex flex-col md:flex-row  gap-8 '>
-                <div className='bg-zinc-800 rounded p-4 h-[50vh] lg:h-[85vh] lg:w-3/6 flex items-center justify-center'>
+            <div className='px-5 md:px-12 py-8 bg-zinc-900 flex flex-col md:flex-row  gap-8 '>
+                <div className='bg-zinc-800 rounded md:p-4 p-6 h-[60vh] md:h-[85vh] md:w-3/6 flex flex-col md:flex-row items-start justify-center'>
                     {" "}
-                    <img src={Data.url} alt="image" className='h-[40vh] lg:h-[70vh] w-[65vh] rounded'/>
+                    <div className='w-full md:w-6/6 '>
+                    {" "}
+                        <img src={Data.url} alt="image" className='md:ml-4 ml-0 h-[50vh] md:h-[78vh] w-full md:w-5/6 rounded'/>
+                    </div>
+                    {isLoggedIn === true && role === "user" && (
+                        <div className='flex flex-row md:flex-col items-center justify-center max-md:w-full gap-6 mt-4 ml-0 '>
+                            <button className='bg-white rounded-full text-base md:text-3xl p-2 text-red-500 flex items-center justify-center'
+                                onClick={handleFavourite}>
+                                    <FaHeart/>{" "}
+                                    <span className='ms-4 block md:hidden '>Favourites</span>
+                            </button>
+                            <button className='bg-blue-500 rounded-full text-base md:text-3xl p-2 text-white flex items-center justify-center'>
+                                <FaCartShopping/>{" "}
+                                <span className='ms-4 block md:hidden '>Add to Cart</span>
+                            </button>
+                    </div>
+                    )}
+                    {isLoggedIn === true && role === "admin" && (
+                        <div className='flex flex-row md:flex-col items-center justify-center max-md:w-full gap-6 mt-4 ml-0 '>
+                            <button className='bg-white rounded-full text-xl font-semibold md:text-3xl p-2 max-md:px-4 flex items-center justify-center'>
+                                <MdModeEditOutline />{" "}
+                                <span className='ms-4 block md:hidden '>Edit</span>
+                            </button>
+                            <button className='text-red-500 rounded-full text-xl font-semibold md:text-3xl p-2 max-md:px-4 bg-white flex items-center justify-center'>
+                                <MdDeleteOutline />{" "}
+                                <span className='ms-4 block md:hidden '>Delete</span>
+                            </button>
+                    </div>
+                    )}
+                    
+                    
                 </div>
-                <div className='p-4 lg:w-3/6'>
+                <div className='p-4 md:w-3/6'>
                     <h1 className='text-4xl font-semibold text-zinc-100'>{Data.title}</h1>
                     <p className='mt-1 text-zinc-300'>By {Data.author}</p>
                     <p className='mt-4 text-xl text-zinc-400'>Rs. {Data.desc}</p>
